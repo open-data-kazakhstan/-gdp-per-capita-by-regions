9cafb645695c76835ea2ccf5378b4355bb2b7864
MZhiyenbayev <mzhiyenbayev@one.kz>
2022-05-06 19:07:25 +0600

    added scripts, datapackage.json and readme.md, also actions.yml

---
+++ scripts/process.py
@@ -0,0 +1,17 @@
+import json
+import requests
+import csv
+
+r = requests.get('https://taldau.stat.gov.kz/ru/Api/GetIndexData/2709380?period=7&dics=67')
+
+raw_dt = json.loads(r.text)
+
+with open('data/output.csv', 'w', newline='') as file:
+    fieldnames = ['region', 'date', 'value']
+    writer = csv.writer(file)
+    writer.writerow(fieldnames)
+    for i in raw_dt:
+        for j in range(len(i["periods"])):
+            dt = '{year}-{month}-{day}'.format(year = i["periods"][j]['date'].split(".")[2], month = i["periods"][j]['date'].split(".")[1], day = i["periods"][j]['date'].split(".")[0])
+            writer.writerow((i["termNames"][0],dt,i["periods"][j]['value'].split(".")[0]))
+            
